e3f88747cf7deec99cce62503a669027b323a3c3
cleo412 <semenenko412@gmail.com>
2023-06-30 20:36:16 +0300

    fix:spinner color in Safari

--- src/components/PageSpinner.vue
+++ src/components/PageSpinner.vue
@@ -23,11 +23,12 @@ export default {
 
 .spinner__body {
   position: absolute;
-  bottom: 30%;
+  bottom: 15%;
   right: 50%;
   width: 16px;
   height: 16px;
   border-radius: 4px;
+  color: var(--vt-c-active-4);
   box-shadow: 0 30px, 0 -30px;
   transform: translateY(30px);
   animation: cube 2s ease infinite;
@@ -43,30 +44,28 @@ export default {
   width: 16px;
   height: 16px;
   border-radius: 4px;
+  color: var(--vt-c-active-4);
   box-shadow: 0 30px, 0 -30px;
   animation: cube 2s 0.2s ease infinite;
 }
 
 .spinner__body::before {
   left: 60px;
-  animation-delay: 0.4s;
+  animation: cube 2s 0.4s ease infinite;
 }
 
 @keyframes cube {
   0% {
     top: 0;
-    color: var(--vt-c-active-2);
     background: var(--vt-c-active-2);
   }
   50% {
     top: 30px;
-    color: var(--vt-c-active-4);
     background: var(--vt-c-active-4);
   }
   100% {
     top: 0;
-    color: var(--vt-c-active-6);
-    background: var(--vt-c-active-6);
+    background: var(--vt-c-active-4);
   }
 }
 </style>
