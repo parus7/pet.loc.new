19d888ca87f7edcda377bdc55c275027fae9b1d3
cleo412 <semenenko412@gmail.com>
2023-10-03 19:24:17 +0300

    fix: go to employee's archive card

--- src/view/PageArchiveCard.vue
+++ src/view/PageArchiveCard.vue
@@ -1,37 +1,37 @@
 <template>
-<!--  <PageCard-->
-<!--    :employee="employee"-->
-<!--    :isEdit="isEdit"-->
-<!--    :isMain="isMain"-->
-<!--    @editOk="isEdit=true"-->
-<!--    @editNo="isEdit=false"-->
-<!--  />-->
+  <PageCard
+    :employee="employee"
+    :isEdit="isEdit"
+    :isMain="isMain"
+    @editOk="isEdit=true"
+    @editNo="isEdit=false"
+  />
 </template>
 
 <script>
-// import PageCard from "@/components/PageCard.vue";
-// import { mapState } from "pinia";
-// import { useEmplStore } from "@/stores/EmplStore";
-//
-// export default {
-//   components: { PageCard },
-//
-//   data() {
-//     return {
-//       employee: {},
-//       isEdit: false,
-//       isMain: true,
-//     };
-//   },
-//
-//   created() {
-//     const paramsId = this.$route.params.id;
-//     this.employee = { ...this.getEmployeeById("archive", paramsId) };
-//   },
-//
-//   computed: {
-//     ...mapState(useEmplStore, ["getEmployeeById"])
-//   }
-// };
+import PageCard from "@/components/PageCard.vue";
+import { mapState } from "pinia";
+import { useEmplStore } from "@/stores/EmplStore";
+
+export default {
+  components: { PageCard },
+
+  data() {
+    return {
+      employee: {},
+      isEdit: false,
+      isMain: true
+    };
+  },
+
+  created() {
+    const paramsId = this.$route.params.id;
+    this.employee = { ...this.getEmployeeById("archive", paramsId) };
+  },
+
+  computed: {
+    ...mapState(useEmplStore, ["getEmployeeById"])
+  }
+};
 </script>
 
